8ec5e2fbef3715d91a004e97dbd97fb80cea5062
Mark Lowell <markmlowell@gmail.com>
2018-06-06 02:50:20 -0700

    Initial Commit

---
+++ Convert/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Convert
+//
+//  Created by Mark Lowell on 6/6/18.
+//  Copyright © 2018 Mark Malstrom. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
